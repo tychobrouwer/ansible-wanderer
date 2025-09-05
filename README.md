99a7a4503799821cf0132483bc07a06ade429c35
Tycho Brouwer <tycho.tbrouwer@gmail.com>
2025-09-05 14:13:46 +0200

    initial try

---
+++ README.md
@@ -0,0 +1,37 @@
+Install and configure wanderer
+=========
+
+The role installs and configures Wanderer.
+
+Role Variables
+--------------
+
+Example Playbook
+----------------
+
+```yaml
+- hosts: all
+  vars:
+    wanderer_meili_master_key: your_meilisearch_master_key
+    wanderer_pocketbase_encryption_key: your_pocketbase_encryption_key
+
+  roles:
+    - role: tychobrouwer.wanderer
+
+    - role: ansible-wanderer
+      wanderer_user: wanderer
+      wanderer_group: wanderer
+      wanderer_install_dir: /opt/wanderer
+      wanderer_disable_signup: true
+      wanderer_private_instance: true
+```
+
+License
+-------
+
+BSD
+
+Author Information
+------------------
+
+An optional section for the role authors to include contact information, or a website (HTML is not allowed).
---
+++ templates/launch-wanderer.sh
@@ -0,0 +1,24 @@
+trap "kill 0" EXIT
+
+# Required configuration
+export ORIGIN=http://localhost:3000
+export MEILI_URL=http://127.0.0.1:7700
+export MEILI_MASTER_KEY={{ wanderer_meili_master_key }}
+export PUBLIC_POCKETBASE_URL=http://127.0.0.1:8090
+export PUBLIC_VALHALLA_URL=https://valhalla1.openstreetmap.de
+export POCKETBASE_ENCRYPTION_KEY={{ wanderer_pocketbase_encryption_key }}
+
+# Optional configuration
+export MEILI_NO_ANALYTICS=true
+# export BODY_SIZE_LIMIT=Infinity
+export PUBLIC_DISABLE_SIGNUP={{ wanderer_disable_signup }}
+export PUBLIC_PRIVATE_INSTANCE={{ wanderer_private_instance }}
+# export UPLOAD_FOLDER=/app/uploads
+# export UPLOAD_USER=
+# export UPLOAD_PASSWORD=
+
+cd {{ wanderer_install_dir }}/search && ./meilisearch --master-key $MEILI_MASTER_KEY &
+cd {{ wanderer_install_dir }}/db && ./pocketbase serve &
+cd {{ wanderer_install_dir }}/web && node build &
+
+wait
